e5b0b6799d9fe7652db43bbba24f8ad726b95a4d
Peter Chen <chen.peter7@gmail.com>
2020-07-27 15:13:27 -0400

    added back redirect to login if not logged in

--- input.php
+++ input.php
@@ -45,7 +45,7 @@ if (isset($_SESSION['user']))
                     </div>
 
                     <div class="col-md-3">
-                        <label for="work-area">Search for work area:</label>
+                        <label for="work-area">Work area:</label>
                         <input type="text" list="work-areas" name="work-area" id="work-area"
                             value="<?php if(isset($_GET['work-area']) && !$confirm) echo $_GET['work-area'] ?>"
                             <?php if(empty($_GET['work-area'])) { ?> autofocus <?php }; ?> />
@@ -65,7 +65,7 @@ if (isset($_SESSION['user']))
                     </div>
 
                     <div class="col-md-3">
-                        <label for="date">Enter date worked:</label>
+                        <label for="date">Day of work:</label>
                         <input type="date" name="date" id="date" />
                         <span class="alert-danger"><?= $date_msg; ?></span>
                     </div>
@@ -78,59 +78,66 @@ if (isset($_SESSION['user']))
             <!-- TODO: Save last input as cookie and display -->
         </div>
         <script src="js/inputScript.js"></script>
+        
+        <?php
+        //close bracket from the "if" from before
+        }
+        else {   // not logged in yet
+            header('Location: login.php');  // redirect to the login page
+        }
+        ?>
 
-		<?php
-		//Make a table of all the work areas and populate it (if they don't exist already)
-		$work_areas = array(["Curds", 1000],
-						["curds", 1000],
-						["Garden", 1000],
-						["Hammocks", 1000],
-						["Kettle", 1000],
-						["Pack Help", 1000],
-						["Pack Honcho", 1000],
-						["Seed Racks", 1000],
-						["Seeds", 1000],
-						["Tofu Hut", 1000],
-						["Trays", 1000]
-					);
+        <?php
+        //Make a table of all the work areas and populate it (if they don't exist already)
+        $work_areas = array(["Curds", 1000],
+                        ["curds", 1000],
+                        ["Garden", 1000],
+                        ["Hammocks", 1000],
+                        ["Kettle", 1000],
+                        ["Pack Help", 1000],
+                        ["Pack Honcho", 1000],
+                        ["Seed Racks", 1000],
+                        ["Seeds", 1000],
+                        ["Tofu Hut", 1000],
+                        ["Trays", 1000]
+                    );
 
-		require('model/connect-db.php');
-		$query = "SELECT * FROM `work_areas`";
-		$statement = $db->prepare($query);
-		$statement->execute();	
-		$result = $statement->fetchAll();
-		//The table doesn't exit
-		if (!$result){
-				echo "It doesn't exist!";
-				
-				$query = "CREATE TABLE work_areas(
-				area_name VARCHAR(255) PRIMARY KEY,
-				labor_balance INT NOT NULL )";
-				$statement = $db->prepare($query);
-				$statement->execute();
-				$statement->closeCursor();
-			}
-		foreach ($work_areas as $work_area){
-			$query = "SELECT * FROM `work_areas` WHERE area_name=:area";
-			$statement = $db->prepare($query);
-			$statement->bindValue(':area', $work_area[0]);
-			$statement->execute();
-			$result = $statement->fetch();
-			//The work area doesn't exist
-			if (!$result){
-				$que = "INSERT INTO `work_areas` (area_name, labor_balance) VALUES(:area, :labor)";
-				$state = $db->prepare($que);
-				$state->bindValue(':area', (String)$work_area[0]);
-				$state->bindValue(':labor', (int)$work_area[1]);
-	
-				$r = $state->execute();
-			}
-			else{
-				echo "Yay! The work area exists! </br>";
-			}
-		}
-	}
-?>
-		
+        require('model/connect-db.php');
+        $query = "SELECT * FROM `work_areas`";
+        $statement = $db->prepare($query);
+        $statement->execute();	
+        $result = $statement->fetchAll();
+        //The table doesn't exit
+        if (!$result){
+                echo "It doesn't exist!";
+                
+                $query = "CREATE TABLE work_areas(
+                area_name VARCHAR(255) PRIMARY KEY,
+                labor_balance INT NOT NULL )";
+                $statement = $db->prepare($query);
+                $statement->execute();
+                $statement->closeCursor();
+            }
+        foreach ($work_areas as $work_area){
+            $query = "SELECT * FROM `work_areas` WHERE area_name=:area";
+            $statement = $db->prepare($query);
+            $statement->bindValue(':area', $work_area[0]);
+            $statement->execute();
+            $result = $statement->fetch();
+            //The work area doesn't exist
+            if (!$result){
+                $que = "INSERT INTO `work_areas` (area_name, labor_balance) VALUES(:area, :labor)";
+                $state = $db->prepare($que);
+                $state->bindValue(':area', (String)$work_area[0]);
+                $state->bindValue(':labor', (int)$work_area[1]);
+
+                $r = $state->execute();
+            }
+            else{
+                echo "Yay! The work area exists! </br>";
+            }
+        }
+        ?>
+
     </body>
 </html>
