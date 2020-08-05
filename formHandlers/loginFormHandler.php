a9b1d494339737a03a2871606e50b660d80839ed
Peter Chen <chen.peter7@gmail.com>
2020-08-05 12:42:09 -0400

    fixed bug where user was always sent to profile page when remembered

--- formHandlers/loginFormHandler.php
+++ formHandlers/loginFormHandler.php
@@ -1,15 +1,10 @@
 <!-- Authors: Rowan Dakota and Peter Chen -->
 <?php
-    /** display error message and exit */
-    function reject() {
-        echo "<div style='text-align: center;' class='bg-danger text-white'>The username or password is incorrect</div>";
-        exit();
-    }
     /** Authenticates the login form by checking for the given username and password in the database */
     function authenticate($user, $pwd) {
         // if the values contain only alphanumeric data, display error and exit
         if(!ctype_alnum($user) || !ctype_alnum($pwd))
-            reject();
+            return false;
 
         require('model/connect-db.php');
         $hashPwd = md5($pwd); // hash the password
@@ -23,36 +18,17 @@
         
         $statement->execute();
         
-        global $result;
         $result = $statement->fetch();
-        
+        $statement->closeCursor();
         // This is the case where the username/password is incorrect
         if (!$result){
-            $statement->closeCursor();
-            reject();
+            echo "<div style='text-align: center;' class='bg-danger text-white'>The username or password is incorrect</div>";
+            return false;
         }
         // if the statement is correctly fetched, then it goes to the profile page
         else{
-            $statement->closeCursor();
-            // starts a session object
-            session_start();
-            $_SESSION['user'] = $user;
-            // TODO: change this so it stores an encoding of the password later.
-            // something like, $_SESSION['pwd'] = md5($pwd);
-            $_SESSION['pwd'] = $hashPwd;
-
-            // puts cookie in user's browser if 'Remember me' option is checked
-            if(isset($_POST['remember']) && $_POST['remember'] == "1")
-            {
-                setcookie('username', $user, time() + 86400); // set for one day
-                setcookie('password', $hashPwd, time() + 86400);
-            }
-
-            // branches to other page
-            header('Location: profile.php');
+            return true;
         }
-
-        $statement->closeCursor();
         
     }
 ?>
@@ -62,12 +38,28 @@
     if (isset($_COOKIE['username']) && isset($_COOKIE['password'])) {
         $username = trim($_COOKIE['username']);
         $password = trim($_COOKIE['password']);
-        authenticate($username, $password);
+        $authorized = authenticate($username, $password);
+        if($authorized) {
+            session_start();
+            $_SESSION['user'] = $username;
+        }
     }
 
     else if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['username']) && isset($_POST['password'])) {	
         $username = trim($_POST['username']);
         $password = trim($_POST['password']);
-        authenticate($username, $password);
+        $authorized = authenticate($username, $password);
+        if($authorized) {
+            session_start();
+            $_SESSION['user'] = $username;
+            // puts cookie in user's browser if 'Remember me' option is checked
+            if(isset($_POST['remember']) && $_POST['remember'] == "1")
+            {
+                setcookie('username', $username, time() + 86400); // set for one day
+                setcookie('password', md5($password), time() + 86400);
+            }
+            // redirects to other page
+            header('Location: profile.php');
+        }
     }
 ?>
