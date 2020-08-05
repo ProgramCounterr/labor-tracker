f57dc12f800fc4dcba911d32ba9524d83f25aeb9
Peter Chen <chen.peter7@gmail.com>
2020-08-05 17:42:41 -0400

    removed echo statement and refactored

--- formHandlers/loginFormHandler.php
+++ formHandlers/loginFormHandler.php
@@ -1,8 +1,9 @@
-<!-- Authors: Rowan Dakota and Peter Chen -->
 <?php
+    // author: Peter Chen
+    
     /** Authenticates the login form by checking for the given username and password in the database */
     function authenticate($user, $pwd) {
-        // if the values contain only alphanumeric data, display error and exit
+        // if the values do not contain only alphanumeric data, return false
         if(!ctype_alnum($user) || !ctype_alnum($pwd))
             return false;
 
@@ -21,45 +22,12 @@
         $result = $statement->fetch();
         $statement->closeCursor();
         // This is the case where the username/password is incorrect
-        if (!$result){
-            echo "<div style='text-align: center;' class='bg-danger text-white'>The username or password is incorrect</div>";
+        if (!$result)
             return false;
-        }
+
         // if the statement is correctly fetched, then it goes to the profile page
-        else{
+        else
             return true;
-        }
         
-    }
-?>
-
-<?php
-    // The isset makes sure that there is something in the text fields
-    if (isset($_COOKIE['username']) && isset($_COOKIE['password'])) {
-        $username = trim($_COOKIE['username']);
-        $password = trim($_COOKIE['password']);
-        $authorized = authenticate($username, $password);
-        if($authorized) {
-            session_start();
-            $_SESSION['user'] = $username;
-        }
-    }
-
-    else if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['username']) && isset($_POST['password'])) {	
-        $username = trim($_POST['username']);
-        $password = trim($_POST['password']);
-        $authorized = authenticate($username, $password);
-        if($authorized) {
-            session_start();
-            $_SESSION['user'] = $username;
-            // puts cookie in user's browser if 'Remember me' option is checked
-            if(isset($_POST['remember']) && $_POST['remember'] == "1")
-            {
-                setcookie('username', $username, time() + 86400); // set for one day
-                setcookie('password', md5($password), time() + 86400);
-            }
-            // redirects to other page
-            header('Location: profile.php');
-        }
     }
 ?>
