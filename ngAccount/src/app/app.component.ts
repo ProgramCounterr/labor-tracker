401d29315dad66b4db3957a92aa3d35b0877d8db
Peter Chen <chen.peter7@gmail.com>
2020-08-05 22:50:39 -0400

    integrated settings into account page

--- ngAccount/src/app/app.component.ts
+++ ngAccount/src/app/app.component.ts
@@ -32,4 +32,27 @@ export class AppComponent implements OnInit {
     this.authenticate();
   }
 
+  // settings stuff
+  fontSize = 16;       /* set default */
+	is_dark = false;
+	show_settings = false;
+	fontFamily = 'Times New Roman';
+	fontSizes = [
+		{ name: 'small', size: '12' },
+		{ name: 'medium', size: '16' },
+		{ name: 'large', size: '25' }
+	];
+	fontFamilies = [
+		{ name: 'Times New Roman' },
+		{ name: 'Helvetica' },
+		{ name: 'Lucida Console' },
+		{ name: 'Brush Script MT' }
+	];
+	toggleDark(){
+		this.is_dark = !this.is_dark;
+	}
+	toggleSettings(){
+		this.show_settings = !this.show_settings;
+  }
+
 }
